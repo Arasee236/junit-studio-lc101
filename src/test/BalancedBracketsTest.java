f425bc8c54751ca8b7ba5fc854c2e350cec7b392
Arasi Arumugam <ezhil.arum@gmail.com>
2021-08-11 20:54:32 -0500

    Test Cases for Studio 6 Completed

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,65 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void matchingBracketsWithStringReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void emptyStringWithoutBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void matchedBracketsWithinStringReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void matchedBracketsWithMultiStringsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode[Rocks]; CoderG[irl]"));
+    }
+    @Test
+    public void matchedBracketsWithNoStringsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[                      ][]"));
+    }
+
+    @Test
+    public void matchedBracketswithMultiLineStringsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Launch[Code] rocks!" + "\n Coder[]Girl" + "\n Java Web[DevelopmentTrack]"));
+    }
+
+    //test cases that return false
+
+    @Test
+    public void singleBracketWithNoStringReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void singleBracketwithinStringReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code"));
+    }
+    @Test
+    public void misMatchedBracketsWithNoStringsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]][][]["));
+    }
+
+    @Test
+    public void misMatchedBracketsWithMultipleStringsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Launchcode] Rocks][]"));
+    }
+
+    @Test
+    public void misMatchedBracketsWithMultiLineStringsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Launhcode ]][ ][[" +" \n Coder]][]Girl][" + "\n Java][] Web Development" + "\n Rocks!!![]]"));
+
+    }
 
 }
